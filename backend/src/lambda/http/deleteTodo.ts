6b20c22b029e4a6e73f8404e2f2c4207ee466551
Daniel Vasconcelos <daniel.oliveiravas@gmail.com>
2019-09-07 11:51:46 -0300

    Add delete todo implementation

--- backend/src/lambda/http/deleteTodo.ts
+++ backend/src/lambda/http/deleteTodo.ts
@@ -1,11 +1,29 @@
 import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import { createDynamoDBClient } from '../utils'
+
+const docClient = createDynamoDBClient();
+
+const TODOS_TABLE = process.env.TODOS_TABLE;
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    const todoId = event.pathParameters.todoId
+  const todoId = event.pathParameters.todoId
 
-    // TODO: Remove a TODO item by id
-    return undefined
-  }
-)
+  await docClient.delete({
+    TableName: TODOS_TABLE,
+    Key: {
+      'todoId': todoId
+    },
+    ConditionExpression: 'todoId = :todoId',
+    ExpressionAttributeValues: {
+      ':todoId': todoId
+    }
+  }).promise();
+
+
+  return {
+    statusCode: 200,
+    body: ''
+  };
+}
