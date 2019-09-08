fe76e7b0ecf5c4175b51467d28980ebab02ed220
Daniel Vasconcelos <daniel.oliveiravas@gmail.com>
2019-09-08 14:28:43 -0300

    feat: add cors configuration

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -1,7 +1,9 @@
 import 'source-map-support/register';
 import * as uuid from 'uuid';
+import * as middy from 'middy';
+import { cors } from 'middy/middlewares';
 
-import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda';
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest';
 import { createDynamoDBClient } from '../utils';
@@ -9,7 +11,7 @@ const docClient = createDynamoDBClient();
 
 const TODOS_TABLE = process.env.TODOS_TABLE;
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const newTodo: CreateTodoRequest = JSON.parse(event.body);
   const todoId = uuid.v4();
 
@@ -27,9 +29,12 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
 
   return {
     statusCode: 201,
-    headers: {
-      'Access-Control-Allow-Origin': '*'
-    },
     body: JSON.stringify(newItem)
   }
-}
+});
+
+handler.use(
+  cors({
+    credentials: true
+  })
+);
