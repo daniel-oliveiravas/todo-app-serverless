da21ef85c514e0bc578e0408ebcbfcec5d598fb1
Daniel Vasconcelos <daniel.oliveiravas@gmail.com>
2019-09-08 14:29:15 -0300

    fix: check todo lists before rendering

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,8 +1,8 @@
-const apiId = '...'
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+const apiId = '45ypl1pyll';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`;
 
 export const authConfig = {
-  domain: '...',
-  clientId: '...',
+  domain: 'token-authenticator.auth0.com',
+  clientId: 'VlQB2la27rwB5oO0oC01p5tSdEHuNfyP',
   callbackUrl: 'http://localhost:3000/callback'
 }
